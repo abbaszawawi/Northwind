1f93848411a162a6ce189028c88a39e9d446ad9c
Abbas Zawawi <zawawi@pacbell.net>
2020-04-07 03:26:27 -0400

    Added API Tests

---
+++ Northwind.API.Tests/Common/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Northwind.API.Tests.Common
+{
+    public class Utilities
+    {
+        public static async Task<T> GetResponseContent<T>(HttpResponseMessage response)
+        {
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(stringResponse);
+        }
+    }
+}
